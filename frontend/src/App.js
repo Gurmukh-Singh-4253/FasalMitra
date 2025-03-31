8d40353e1b0d32bc75721775763d04989e7b845e
Gurmukh-Singh-4253 <gurmukh050304@gmail.com>
2025-03-31 12:30:31 +0530

    final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,11 @@
 import React from 'react';
 import DiseaseDetection from './components/DiseaseDetection';
 import './App.css';
-import TranslatePage from './components/TranslatePage';
 
 function App() {
   return (
     <div className="App">
       <DiseaseDetection />
-      <TranslatePage />
     </div>
   );
 }
